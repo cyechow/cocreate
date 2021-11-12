4e4cfbd97854929e2c194c3918d745aba9a02cde
Edith Chow <edith.chow@arup.com>
2021-11-12 12:50:26 -0500

    Printing out scripts folder env var.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,8 @@ source /etc/profile
 
 eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
 
+echo "${SCRIPTS_FOLDER}"
+
 set -o errexit
 
 cd $SCRIPTS_FOLDER
