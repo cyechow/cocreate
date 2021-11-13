360298c877e4af446c432e4eb93df59992510ceb
Edith Chow <edith.chow@arup.com>
2021-11-12 22:26:30 -0500

    Updated website title to show that it's customized.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-echo 'Copying over environment variables...'
-
-# eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
-
-echo "${SCRIPTS_FOLDER}"
-echo "${MONGO_URL}"
-
 set -o errexit
 
 cd $SCRIPTS_FOLDER
